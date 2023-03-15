79cd74bd49b10475133d7ca3ebca7dd80db6e891
Arutoria <17620725494@163.com>
2023-03-15 17:06:49 +0800

    :necktie: 增加表单的自动完成提示

--- src/designer/utils/complete.js
+++ src/designer/utils/complete.js
@@ -1,22 +1,68 @@
 import { javascriptLanguage } from "@codemirror/lang-javascript";
+import { seletedSchema } from "../core/select";
+
 export const ctxComplete = javascriptLanguage.data.of({
   autocomplete: function (context) {
     let word = context.matchBefore(/.*ctx\./);
     if (word && word.text.trim() === "ctx.") {
-      return {
-        from: word.to,
-        options: [
+      let hintOptions = [
+        {
+          label: "props",
+          type: "variable",
+          detail: "组件属性"
+        },
+        {
+          label: "refs",
+          type: "variable",
+          detail: "表单引用对象"
+        },
+        {
+          label: "emit",
+          type: "function",
+          detail: "触发自定义事件"
+        }
+      ];
+
+      const { options } = seletedSchema.value;
+      if (options && options.formRefName) {
+        hintOptions = [
+          ...hintOptions,
           {
-            label: "props",
+            label: `refs.${options.formRefName}`,
             type: "variable",
-            info: "组件属性"
+            detail: "表单实例对象"
           },
           {
-            label: "emit",
+            label: `refs.${options.formRefName}.validate`,
             type: "function",
-            info: "触发自定义事件"
+            detail: "触发表单验证, 同 validateFields"
+          },
+          {
+            label: `refs.${options.formRefName}.validateFields`,
+            type: "function",
+            detail: "触发表单验证"
+          },
+          {
+            label: `refs.${options.formRefName}.clearValidate`,
+            type: "function",
+            info: "移除表单项的校验结果。传入待移除的表单项的 name 属性或者 name 组成的数组，如不传则移除整个表单的校验结果"
+          },
+          {
+            label: `refs.${options.formRefName}.scrollToField`,
+            type: "function",
+            detail: "滚动到对应字段位置"
+          },
+          {
+            label: `refs.${options.formRefName}.resetFields`,
+            type: "function",
+            info: "对整个表单进行重置，将所有字段值重置为初始值并移除校验结果"
           }
-        ]
+        ];
+      }
+
+      return {
+        from: word.to,
+        options: hintOptions
       };
     }
 
