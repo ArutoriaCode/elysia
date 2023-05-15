0f5d10df84e28a2f97f593c10278df6d3aceefd3
Arutoria <17620725494@163.com>
2023-05-15 20:57:19 +0800

    :bug: 修复问题

--- src/designer/ElysiaBuilds.vue
+++ src/designer/ElysiaBuilds.vue
@@ -1,8 +1,17 @@
 <script setup>
+import { nextTick, ref, watch } from 'vue';
 import tabContent from './core/tabs'
+
+const refresh = ref(true)
+watch(tabContent, () => {
+  refresh.value = false
+  nextTick(() => {
+    refresh.value = true
+  })
+})
 </script>
 <template>
-  <div class="elysia-builds-render">
+  <div class="elysia-builds-render" v-if="refresh">
     <builds-render :schema-json="tabContent"></builds-render>
   </div>
 </template>
